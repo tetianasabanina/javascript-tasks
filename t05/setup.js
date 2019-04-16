5ebc35db65bdc5241f238261bbef4cf2e0f14251
markkuruonavaara <markku.ruonavaara@gmail.com>
2019-04-16 06:52:53 +0300

    Pikku muutos t5-suorituskoneeseen

--- t05/setup.js
+++ t05/setup.js
@@ -10,24 +10,24 @@ function cls(e) {
     document.querySelector('#output').innerHTML = '';
 }
 
-function run(f) {
+function run(f, ...args) {
     println(f.name);
-    return f;
+    return f(...args);
 }
 
 const handlers = {
-    f1: () => run(parilliset)(),
-    f2: () => run(yhdestaJohonkin)(12),
-    f3: () => run(jostainJohonkin)(5, 17),
-    f4: () => run(tulostaTahtia)(9),
-    f5: () => run(tulostaNelio)(4),
-    f6: () => run(tulostaSuorakulmio)(17, 3),
-    f7: () => run(tulostaKolmio)(5),
-    f8: () => println(run(lukusarjanSumma)(100)),
-    f9: () => println(run(kertoma)(10)),
-    f10: () => run(ajaKaikki)(),
-    f11: () => run(println)(rekursiivinenSumma(200)),
-    f12: () => run(println)(rekursiivinenKertoma(12))
+    f1: () => run(parilliset),
+    f2: () => run(yhdestaJohonkin, 12),
+    f3: () => run(jostainJohonkin, 5, 17),
+    f4: () => run(tulostaTahtia, 9),
+    f5: () => run(tulostaNelio, 4),
+    f6: () => run(tulostaSuorakulmio, 17, 3),
+    f7: () => run(tulostaKolmio, 5),
+    f8: () => println(run(lukusarjanSumma, 100)),
+    f9: () => println(run(kertoma, 10)),
+    f10: () => run(ajaKaikki),
+    f11: () => println(run(rekursiivinenSumma, 200)),
+    f12: () => println(run(rekursiivinenKertoma, 12))
 }
 
 for (const key in handlers) {

48e353f19dedbd595cd4c297888137f6029a2461
Sabanina Tetiana <s1900565@edu.bc.fi>
2019-05-13 11:01:27 +0300

    time calculator works

--- t06/main.js
+++ t06/main.js
@@ -2,8 +2,9 @@
 
 function duration(from, to) { //get start and finish of period and returns the duration of period in JS object.
     var timer = to - from; // duration in msec
+    console.log(timer);
     
-    var timerSec = Math.floor(timer / 1000); // duration in sec 
+    var timerSec = Math.round(timer / 1000); // duration in sec 
     var sec = timerSec % 60; // extracts seconds
    
     var timerMin = Math.floor(timerSec / 60); // duration in minutes
@@ -32,3 +33,11 @@ var timeForEvent;
 console.log("event: "+event);
 duration(today, event);
 console.log (timeForEvent);
+// document.querySelectorAll(`[data-key='${d}']`).innerHTML = timeForEvent[0];
+// var test = document.
+//console.log(timeForEvent['d']);
+//console.log(document.querySelector(`[data-key='${"d"}']`).innerHTML);
+document.querySelector(`[data-key='${"d"}']`).innerHTML = timeForEvent['d'];
+document.querySelector(`[data-key='${"h"}']`).innerHTML = timeForEvent['h'];
+document.querySelector(`[data-key='${"m"}']`).innerHTML = timeForEvent['m'];
+document.querySelector(`[data-key='${"s"}']`).innerHTML = timeForEvent['s'];

ac1ccc9aef65b0096bd3ec74cc02be96ae94d676
Sabanina Tetiana <s1900565@edu.bc.fi>
2019-05-19 12:08:44 +0300

    config form sends data to JS

---
+++ t06/config.js
@@ -0,0 +1,29 @@
+function myFunction() {
+    
+    var otsikko = document.getElementById("otsikko").value;
+    console.log("otsikko "+otsikko);
+    var pvm = document.getElementById('pvm').value;
+    console.log("pvm"+pvm);
+    var aika = document.getElementById('aika').value;
+    console.log(aika);
+    var viesti = document.getElementById('viesti').value;
+    console.log(viesti);
+    //let message;
+    
+    /*if (!IsValidJSONString(syote)) {
+        message = 'virheellinen JSON-muoto';
+    } else {
+        message = 'JSON-merkkijono muodostetaan... ';
+    }
+
+    document.getElementById('message').innerHTML = message;
+    document.getElementById('form').classList.add('muuttunut');
+    setTimeout(poista, 3000);
+    muodostaOlio(syote);
+}
+
+function poista() {
+    document.getElementById('form').classList.remove('muuttunut');
+    document.getElementById('message').innerHTML = "";*/
+//}
+}

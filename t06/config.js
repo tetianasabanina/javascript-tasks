7c1e34329dcd4c2cd6b0f7fa2a214a81bfb1f754
Sabanina Tetiana <s1900565@edu.bc.fi>
2019-05-19 14:55:58 +0300

    function preventSubmit added

--- t06/config.js
+++ t06/config.js
@@ -1,28 +1,23 @@
 function saadaAsetukset() {
-    
+    preventSubmit();
     var otsikko = document.getElementById("otsikko").value;
-    //console.log("otsikko "+otsikko);
+    console.log("otsikko "+otsikko);
     localStorage.setItem('formOtsikko', otsikko);
     var pvm = document.getElementById('pvm').value;
     localStorage.setItem('formPvm', pvm);
-    //console.log("pvm"+pvm);
+    console.log("pvm"+pvm);
     var aika = document.getElementById('aika').value;
-    //console.log(aika);
+    console.log(aika);
     localStorage.setItem('formAika', aika);
     var viesti = document.getElementById('viesti').value;
-    //console.log(viesti);
+    console.log(viesti);
     localStorage.setItem('formViesti', viesti);
 }
-/*
-function validateForm() {
-    var x = document.getElementById("pvm").value;
-    const tänään = new Date();
-    if (x == tänään) {
-      alert("Aika on ohi!");
-      return false;
-    }
-  }*/
+function preventSubmit() {
+document.getElementById("btn").addEventListener("click", function(event){
+    event.preventDefault()
+  });
+}
 
-  /*function varastoon() {
-      localStorage.setItem('otsikko', 'formOtsikko');
-  }*/
+
+

6fac55fb98396df695d086d8c569f8f77bbaf8c0
Sabanina Tetiana <s1900565@edu.bc.fi>
2019-05-19 16:35:56 +0300

    created object to local storage + small improvments

--- t06/config.js
+++ t06/config.js
@@ -1,16 +1,9 @@
 function saadaAsetukset() {
     preventSubmit();
     var otsikko = document.getElementById("otsikko").value;
-    console.log("otsikko "+otsikko);
-    //localStorage.setItem('formOtsikko', otsikko);
     var pvm = new Date(document.getElementById('pvm').value);
-    console.log("pvm"+pvm);
     pvm = pvm + document.getElementById('aika').value;
-    console.log("+ " + pvm);
-    //localStorage.setItem('formPvm', pvm);
     var viesti = document.getElementById('viesti').value;
-    console.log(viesti);
-    //localStorage.setItem('formViesti', viesti);
     var olio = muodostaOlio(otsikko, pvm, viesti);
     var jsonOlio = JSON.stringify(olio);
     localStorage.setItem('asetukset', jsonOlio);

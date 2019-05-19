8f70b0fabf4d3bfc17431eb70813dcdb8fda2bd0
Sabanina Tetiana <s1900565@edu.bc.fi>
2019-05-19 12:34:04 +0300

    saving data to Local Storage

--- t06/config.js
+++ t06/config.js
@@ -1,29 +1,28 @@
-function myFunction() {
+function saadaAsetukset() {
     
     var otsikko = document.getElementById("otsikko").value;
-    console.log("otsikko "+otsikko);
+    //console.log("otsikko "+otsikko);
+    localStorage.setItem('formOtsikko', otsikko);
     var pvm = document.getElementById('pvm').value;
-    console.log("pvm"+pvm);
+    localStorage.setItem('formPvm', pvm);
+    //console.log("pvm"+pvm);
     var aika = document.getElementById('aika').value;
-    console.log(aika);
+    //console.log(aika);
+    localStorage.setItem('formAika', aika);
     var viesti = document.getElementById('viesti').value;
-    console.log(viesti);
-    //let message;
-    
-    /*if (!IsValidJSONString(syote)) {
-        message = 'virheellinen JSON-muoto';
-    } else {
-        message = 'JSON-merkkijono muodostetaan... ';
+    //console.log(viesti);
+    localStorage.setItem('formViesti', viesti);
+}
+/*
+function validateForm() {
+    var x = document.getElementById("pvm").value;
+    const tänään = new Date();
+    if (x == tänään) {
+      alert("Aika on ohi!");
+      return false;
     }
+  }*/
 
-    document.getElementById('message').innerHTML = message;
-    document.getElementById('form').classList.add('muuttunut');
-    setTimeout(poista, 3000);
-    muodostaOlio(syote);
-}
-
-function poista() {
-    document.getElementById('form').classList.remove('muuttunut');
-    document.getElementById('message').innerHTML = "";*/
-//}
-}
+  /*function varastoon() {
+      localStorage.setItem('otsikko', 'formOtsikko');
+  }*/
